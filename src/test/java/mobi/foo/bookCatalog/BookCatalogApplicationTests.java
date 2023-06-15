7670cc0f69183fae2edd0077174cd7220dbe5d11
samermelhem02 <samermelhemm@gmail.com>
2023-06-15 16:06:04 +0300

    initial commit

---
+++ src/test/java/mobi/foo/bookCatalog/BookCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mobi.foo.bookCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
